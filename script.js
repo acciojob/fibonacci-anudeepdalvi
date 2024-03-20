d697c7954116978bf7eab67824c7cc02859e956c
Anudeep Dalvi <anudeepdalvi09@gmail.com>
2024-03-20 13:20:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
 // your code here
+	if (num === 0) return 0;
+
+	let prev1 = 0;
+	let prev2 = 1;
+	let curr;
+
+	for (let i = 2; i <= n; i++) {
+		curr = prev1 + prev2;
+		prev1 = prev2;
+		prev2 = curr;
+	}
+		return prev2;
 }
+console.log(solution(num));
 
 module.exports = fibonacci;
